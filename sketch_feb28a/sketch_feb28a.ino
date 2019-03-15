5bb52660fb0885e23a5379deff7b25b51ee38808
Ahmed Nasser <ahmednasser2337@gmail.com>
2019-03-15 05:20:56 +0200

    Final Lab1&2

--- sketch_feb28a/sketch_feb28a.ino
+++ sketch_feb28a/sketch_feb28a.ino
@@ -4,18 +4,21 @@ const int MPU_addr=0x68;  // I2C address of the MPU-6050
 int16_t AcX,AcY,AcZ,Tmp,GyX,GyY,GyZ;
 float threshhold =80.0;
 int samplesNo =0;
-int steps=0,maxStep=0,minStep=0;
+int steps=0,maxStep=-1000,minStep=1000;
 int16_t samplesAvg [50];
 int16_t xAvg [50];
-int16_t yAvg [50];
+int16_t yAvg [50];a
 int16_t zAvg [50];
+int TimeD=0;
 int16_t tempx,tempy,tempz=0;
+int counterTime=0;
 bool flag =true;
 int timePassed=0;
 int prevSteps=0;
 float distance=0;
+int Mydelay =300;
 const float Height = 1.70 ;
-const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
+const int rs = 12, en = 11, d4 = 6, d5 = 5, d6 = 4, d7 = 3;
 LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 int turn =1;
 
@@ -60,24 +63,26 @@ void loop() {
   xAvg[samplesNo]=AcX;yAvg[samplesNo]=AcY;zAvg[samplesNo]=AcZ;
   int16_t t =sqrt(((xAvg[samplesNo]-tempx)* (xAvg[samplesNo]-tempx))+ ((yAvg[samplesNo] - tempy)*(yAvg[samplesNo] - tempy)) + ((zAvg[samplesNo] - tempz)*(zAvg[samplesNo]-tempz)));
  if(samplesNo !=0){
- samplesAvg[samplesNo] = (t + samplesAvg[samplesNo-1]* samplesNo)/(samplesNo+1); 
+ samplesAvg[samplesNo] = (t + samplesAvg[samplesNo-1])* samplesNo/(samplesNo+1); 
  }else{
   samplesAvg[samplesNo]=t;
  }
+ TimeD=counterTime*Mydelay;
+
+  // Serial.println(TimeD);
+
   if (samplesAvg[samplesNo]>threshhold )
 {
    if(samplesAvg[samplesNo]>maxStep){
     maxStep=samplesAvg[samplesNo];
   }
-
+TimeD=counterTime*Mydelay;
   if(flag==0){
+ // Serial.println(TimeD);
   steps=steps+1;
-  if(timePassed>=2000){
-  displayFunctions(steps);
-  timePassed=0;
-  }
-  flag=1;
-    threshhold=(maxStep+minStep)/2;
+counterTime =0;
+flag=1;
+    threshhold=(maxStep+minStep)*3/4;
   }
 }
   if (samplesAvg[samplesNo] <threshhold  ){
@@ -89,12 +94,25 @@ void loop() {
   }
   }
 
-
+  Serial.print(samplesAvg[samplesNo]);
+  Serial.print(" ");
+  Serial.print(maxStep);
+  Serial.print(" ");
+  Serial.print(minStep);
+  Serial.print(" ");
+  Serial.println(threshhold);
+  if((2000/Mydelay)<=timePassed){
+  displayFunctions(steps);
+  timePassed=0;
+  }
   samplesNo+=1;
    if(samplesNo==50){
     samplesNo=0;}
-    delay(2);
-    timePassed+=2;
+        timePassed+=1;
+
+    delay(Mydelay);
+    counterTime++;
+
 }
 //called each 2 seconds
 void displayFunctions(int steps){
@@ -124,110 +142,28 @@ void displayFunctions(int steps){
 
   float curDistance = stepsD * (Height /ratio );
   distance += curDistance;
-  float vel= distance/2/60;
+  float vel= curDistance/2/60/60;
   float cal= vel * 1.25;
-
-  if(turn ==1 ){
-   lcd.setCursor(0, 1);
-  lcd.print("steps:"+steps);
+    lcd.clear();
+lcd.setCursor(0, 0);
+  lcd.print("steps:"+String(steps));
+  if(turn ==1 ){   
+   
   }else if(turn ==2){
-
-    lcd.setCursor(0, 1);
-  lcd.print("D(km):"+String(distance));
+ lcd.setCursor(0, 0);
+  lcd.print("D(m):"+String(distance));
   }else if(turn ==3){
- lcd.setCursor(0, 1);
-  lcd.print("v(kmH):"+String(vel));
+         
+
+ lcd.setCursor(0, 0);
+  lcd.print("v(mS):"+String(vel));
     
   }else{
-     lcd.setCursor(0, 1);
+     lcd.setCursor(0, 0);
   lcd.print("c(C/kg/h):"+String(cal));
     turn =0;
   }
   turn++;
-  
-
-
-
-  
-  Serial.println('\n');
-  Serial.print("steps=");
-  Serial.println(steps);
-
-    Serial.println('\n');
-  Serial.print("Distance(km)=");
-  Serial.println(distance);
-
-     Serial.println('\n');
-  Serial.print("velocity(kmH)=");
-  Serial.println(vel);
-
-     Serial.println('\n');
-  Serial.print("calories(C/kg/h)=");
-  Serial.println(cal);
 
 prevSteps = steps;
 }
-
-
-
-
-
-
-/*
-
-/*
-  LiquidCrystal Library - Hello World
-
- Demonstrates the use a 16x2 LCD display.  The LiquidCrystal
- library works with all LCD displays that are compatible with the
- Hitachi HD44780 driver. There are many of them out there, and you
- can usually tell them by the 16-pin interface.
-
- This sketch prints "Hello World!" to the LCD
- and shows the time.
-
-  The circuit:
- * LCD RS pin to digital pin 12
- * LCD Enable pin to digital pin 11
- * LCD D4 pin to digital pin 5
- * LCD D5 pin to digital pin 4
- * LCD D6 pin to digital pin 3
- * LCD D7 pin to digital pin 2
- * LCD R/W pin to ground
- * LCD VSS pin to ground
- * LCD VCC pin to 5V
- * 10K resistor:
- * ends to +5V and ground
- * wiper to LCD VO pin (pin 3)
-
- Library originally added 18 Apr 2008
- by David A. Mellis
- library modified 5 Jul 2009
- by Limor Fried (http://www.ladyada.net)
- example added 9 Jul 2009
- by Tom Igoe
- modified 22 Nov 2010
- by Tom Igoe
- modified 7 Nov 2016
- by Arturo Guadalupi
-
- This example code is in the public domain.
-
- http://www.arduino.cc/en/Tutorial/LiquidCrystalHelloWorld
-
-
-// include the library code:
-
-// initialize the library by associating any needed LCD interface pin
-// with the arduino pin number it is connected to
-
-void setup() {
-
-}
-
-void loop() {
-  // set the cursor to column 0, line 1
-  // (note: line 1 is the second row, since counting begins with 0):
- 
-}
-*/
